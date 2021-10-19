d198050a925cb6616a11ad9317e77055429bdd0a
leoloso <leo@getpop.org>
2021-10-19 09:31:26 +0000

    Added SpecifiedByURL for custom scalars

--- src/TypeResolvers/ScalarType/PhoneNumberScalarTypeResolver.php
+++ src/TypeResolvers/ScalarType/PhoneNumberScalarTypeResolver.php
@@ -13,21 +13,21 @@ use stdClass;
  *
  * @see https://spec.graphql.org/draft/#sec-Scalars.Custom-Scalars
  */
-class EmailScalarTypeResolver extends AbstractScalarTypeResolver
+class PhoneNumberScalarTypeResolver extends AbstractScalarTypeResolver
 {
     public function getTypeName(): string
     {
-        return 'Email';
+        return 'PhoneNumber';
     }
 
     public function getTypeDescription(): ?string
     {
-        return $this->translationAPI->__('Email scalar, such as leo@mysite.com', 'component-model');
+        return $this->translationAPI->__('Phone number scalar, such as +1-212-555-0149', 'component-model');
     }
 
     public function getSpecifiedByURL(): ?string
     {
-        return 'https://datatracker.ietf.org/doc/html/rfc5322#section-3.4.1';
+        return 'https://datatracker.ietf.org/doc/html/rfc3966#section-5.1';
     }
 
     public function coerceValue(string|int|float|bool|stdClass $inputValue): string|int|float|bool|stdClass|Error
@@ -36,8 +36,13 @@ class EmailScalarTypeResolver extends AbstractScalarTypeResolver
             return $error;
         }
 
-        if ($error = $this->validateFilterVar($inputValue, \FILTER_VALIDATE_EMAIL)) {
-            return $error;
+        if (\preg_match('/(\+{1}[0-9]{1,3}[0-9]{8,9})/', $inputValue) !== 1) {
+            return $this->getError(
+                sprintf(
+                    $this->translationAPI->__('The format for type \'%s\' is not right: it must be satisfied via regex /(\+{1}[0-9]{1,3}[0-9]{8,9})/', 'component-model'),
+                    $this->getMaybeNamespacedTypeName()
+                )
+            );
         }
         return $inputValue;
     }
--- src/TypeResolvers/ScalarType/UUIDScalarTypeResolver.php
+++ src/TypeResolvers/ScalarType/UUIDScalarTypeResolver.php
@@ -9,35 +9,37 @@ use PoP\ComponentModel\TypeResolvers\ScalarType\AbstractScalarTypeResolver;
 use stdClass;
 
 /**
- * GraphQL Custom Scalar representing a JSON Object on the client-side,
- * handled via an stdClass object on the server-side
+ * GraphQL Custom Scalar
  *
  * @see https://spec.graphql.org/draft/#sec-Scalars.Custom-Scalars
  */
-class JSONObjectScalarTypeResolver extends AbstractScalarTypeResolver
+class UUIDScalarTypeResolver extends AbstractScalarTypeResolver
 {
     public function getTypeName(): string
     {
-        return 'JSONObject';
+        return 'UUID';
     }
 
     public function getTypeDescription(): ?string
     {
-        return $this->translationAPI->__('Custom scalar representing a JSON Object of unrestricted shape', 'component-model');
+        return $this->translationAPI->__('UUID (universally unique identifier) scalar, such as 25770975-0c3d-4ff0-ba27-a0f98fe9b052', 'component-model');
     }
 
     public function getSpecifiedByURL(): ?string
     {
-        return 'https://datatracker.ietf.org/doc/html/rfc7159';
+        return 'https://datatracker.ietf.org/doc/html/rfc4122';
     }
 
     public function coerceValue(string|int|float|bool|stdClass $inputValue): string|int|float|bool|stdClass|Error
     {
-        if (!($inputValue instanceof stdClass)) {
+        if ($error = $this->validateIsString($inputValue)) {
+            return $error;
+        }
+
+        if (\preg_match('/^{?[A-Z0-9]{8}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{12}}?$/', $inputValue) !== 1) {
             return $this->getError(
                 sprintf(
-                    $this->translationAPI->__('Cannot cast value \'%s\' to type \'%s\'', 'component-model'),
-                    $inputValue,
+                    $this->translationAPI->__('The format for type \'%s\' is not right: it must be satisfied via regex /^{?[A-Z0-9]{8}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{12}}?$/', 'component-model'),
                     $this->getMaybeNamespacedTypeName()
                 )
             );
